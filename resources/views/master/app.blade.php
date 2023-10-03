9986c438ef7f529afe08f5e22c2a3d061e53659b
BocillSibuuk <dimanpodu17@gmail.com>
2023-10-03 14:36:49 +0800

    apa

--- resources/views/master/app.blade.php
+++ resources/views/master/app.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <link rel="stylesheet" href="{{ asset('build/assets/app-cbcad190.css') }}">
 </head>
 
 <body>
